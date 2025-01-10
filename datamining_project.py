8eeee44725e6eb5c9c03341dc64c4613b7bb7733
Ereguof <v0.rddos@gmail.com>
2025-01-10 10:17:38 +0100

    final merge v1

--- datamining_project.py
+++ datamining_project.py
@@ -1,6 +1,7 @@
 # Datamining Project Joris FELZINES - Guillaume MANTZARIDES
 
-
+#dependencies for displaying map
+import folium
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -49,25 +50,6 @@ print(data[data[columns_to_check].isnull().any(axis=1)][columns_to_check])
 # print(data.info())
 # print(data.describe())# Datamining Project Joris FELZINES - Guillaume MANTZARIDES
 
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-#dependencies for displaying map
-import folium
-
-data = pd.read_table("C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data2.csv", sep=",", low_memory=False)
-
-# On supprime certaines colonnes : les dates d'upload et les 3 dernières colonnes quasiment vides
-data.drop(data.columns[[11,12,13,14,15,16,17,18]], axis=1, inplace=True)
-
-# minute_values = [i for i in range(0,60)]
-# data_mask = data["date_taken_minute"] not in [i for i in range(0,60)]
-# data = data[data_mask]
-
-print(data.info())
-print(data.describe())
 
 ##############################
 #AFFICHER LA CARTE ET UN MARQUEUR, POUR LA VOIR L'OUVRIR À LA MAIN DANS UN NAVIGATEUR
