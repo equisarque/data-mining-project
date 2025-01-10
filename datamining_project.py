a4ec233192b6b1d52e13c5ec971dd283cd9faddd
Ereguof <v0.rddos@gmail.com>
2025-01-10 11:11:17 +0100

    finitions clean v2

--- datamining_project.py
+++ datamining_project.py
@@ -7,48 +7,67 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_table("C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data2.csv", sep=",", low_memory=False)
+# data = pd.read_table("../../flickr_data2.csv", sep=",", low_memory=False)
 
-# Clean : on supprime certaines colonnes, les dates d'upload et les 3 dernières colonnes quasiment vides car pas utiles pour l'analyse
-# Certaines lignes ont également des valeurs décalées au niveau des dates de prises de photos, on les remet dans le bon sens
-# Enfin, il manque des données de date pour certaines photos, on les supprime afin de garder une cohérence dans les données car elles sont peu nombreuses (moins de 5 pour 420 000 lignes)
+##############################
+#    NETTOYAGE DES DONNÉES   #
+##############################
+
+# On supprime certaines colonnes, les dates d'upload et les 3 dernières colonnes quasiment vides car pas utiles pour l'analyse
 data.drop(data.columns[[11,12,13,14,15,16,17,18]], axis=1, inplace=True)
-print(len(data))
 
 # On renomme les colonnes avec des espaces
-data.rename(columns={' user': 'user', 
+data.rename(columns={' user': 'user',
                     ' lat': 'lat',
                     ' long': 'long',
                     ' tags': 'tags',
                     ' title': 'title',
-                    ' date_taken_year': 'date_taken_year', 
-                    ' date_taken_month': 'date_taken_month', 
-                    ' date_taken_day': 'date_taken_day', 
-                    ' date_taken_hour': 'date_taken_hour', 
+                    ' date_taken_year': 'date_taken_year',
+                    ' date_taken_month': 'date_taken_month',
+                    ' date_taken_day': 'date_taken_day',
+                    ' date_taken_hour': 'date_taken_hour',
                     ' date_taken_minute': 'date_taken_minute'} ,
                     inplace=True)
 
 # On supprime les duplicats
 data.drop_duplicates(keep='first', inplace=True)
-print(len(data))
 
 # On supprime les lignes ou les valeurs manquantes dérangent
 columns_to_check = ['id', 'user', 'lat', 'long', 'date_taken_year', 'date_taken_month', 'date_taken_day', 'date_taken_hour', 'date_taken_minute']
-data.dropna(subset=columns_to_check)
-print(len(data))
+data.dropna(subset=columns_to_check, inplace=True)
 
-# print les rows avec des valeurs manquantes dans columns_to_check
-print(data[data[columns_to_check].isnull().any(axis=1)][columns_to_check])
+# On remet les dates de prises de photos dans le bon sens
+cols_to_shift = ['date_taken_minute', 'date_taken_hour', 'date_taken_day', 'date_taken_month', 'date_taken_year']
+shifted_order_minute = cols_to_shift[1:] + cols_to_shift[:1]
+shifted_order_hour = cols_to_shift[2:] + cols_to_shift[:2]
+shifted_order_day = cols_to_shift[3:] + cols_to_shift[:3]
+shifted_order_month = cols_to_shift[4:] + cols_to_shift[:4]
 
-# datatmp = data.head(10)
+# Pour chaque ligne, on vérifie si les valeurs sont cohérentes
+for index, row in data.iterrows():
+    if row['date_taken_minute'] > 999:
+        data.loc[index, cols_to_shift] = row[shifted_order_minute].values
 
-# for row in datatmp.iterrows():
-#     print(row)
-#     if row["date_taken_minute"] not in range(0,60):
-#         if row["date_taken_minute"] > 1000:
-#             print('a')
+    if row['date_taken_hour'] > 999:
+        data.loc[index, cols_to_shift] = row[shifted_order_hour].values
 
-# print(data.info())
-# print(data.describe())# Datamining Project Joris FELZINES - Guillaume MANTZARIDES
+    if row['date_taken_day'] > 999:
+        data.loc[index, cols_to_shift] = row[shifted_order_day].values
+
+    if row['date_taken_month'] > 999:
+        data.loc[index, cols_to_shift] = row[shifted_order_month].values
+
+    if row['date_taken_year'] > 2025:
+        data.drop(index, inplace=True)
+
+# On convertit les minutes en int
+data['date_taken_minute'] = data['date_taken_minute'].astype(int)
+
+print(data.info())
+
+# Mettre les données nettoyées dans un fichier csv
+data.to_csv("C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_clean.csv", index=False)
+
 
 
 ##############################
