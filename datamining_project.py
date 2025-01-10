6f0aaf83a897961df41ad546fbfaea6d90e71793
equisarque <guillaume@mantzarides.fr>
2025-01-10 10:08:40 +0100

    marker and map

--- datamining_project.py
+++ datamining_project.py
@@ -5,6 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#dependencies for displaying map
+import folium
+
 data = pd.read_table("C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data2.csv", sep=",", low_memory=False)
 
 # On supprime certaines colonnes : les dates d'upload et les 3 dernières colonnes quasiment vides
@@ -16,3 +19,18 @@ data.drop(data.columns[[11,12,13,14,15,16,17,18]], axis=1, inplace=True)
 
 print(data.info())
 print(data.describe())
+
+##############################
+#AFFICHER LA CARTE ET UN MARQUEUR, POUR LA VOIR L'OUVRIR À LA MAIN DANS UN NAVIGATEUR
+##############################
+
+# my_map = folium.Map(location=(45.75, 4.83))
+
+# folium.CircleMarker(
+#     location=[45.75, 4.83],
+#     tooltip="Click me!",
+#     popup="Mt. Hood Meadows",
+#     radius = 3,
+# ).add_to(my_map)
+
+# my_map.save("map.html")
