b2741b222ff880b37825e5e5006c506209b1ff69
equisarque <guillaume@mantzarides.fr>
2025-01-10 08:51:41 +0100

    init

--- datamining_project.py
+++ datamining_project.py
@@ -1,14 +1,12 @@
 # Datamining Project Joris FELZINES - Guillaume MANTZARIDES
 
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_table("../flickr_data2.csv", sep=",", low_memory=False)
+data = pd.read_table("../../flickr_data2.csv", sep=",", low_memory=False)
 
 print(data.head())
 print(data.info())
 print(data.describe())
-
-# print all values of last three columns that aren't NaN
-print(data['haspeople'].value_counts())
