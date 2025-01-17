b577da39817f7a7085691c36532d448f80238f04
Ereguof <v0.rddos@gmail.com>
2025-01-17 08:41:44 +0100

    ajout du sample

--- datamining_project.py
+++ datamining_project.py
@@ -8,6 +8,7 @@ import seaborn as sns
 
 csv_file = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data2.csv"
 csv_file_clean = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_clean.csv"
+csv_file_clean_sample = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_clean-SAMPLE.csv"
 
 def cleaning(csv_file, csv_file_clean):
     data = pd.read_table(csv_file, sep=",", low_memory=False)
@@ -69,7 +70,7 @@ def cleaning(csv_file, csv_file_clean):
 
 # cleaning(csv_file, csv_file_clean)
 
-data = pd.read_table(csv_file_clean, sep=",", low_memory=False)
+data = pd.read_table(csv_file_clean_sample, sep=",", low_memory=False)
 
 #################################AFFICHAGE AVEC SEABORN
 # cols=['lat','long']
@@ -93,3 +94,33 @@ my_map = folium.Map(location=(45.75, 4.83))
 for i in range(len(data)):
     folium.CircleMarker(location=[data.at[i,"lat"], data.at[i,"long"]], tooltip="Afficher titre", popup=data.at[i,"title"], radius = 3).add_to(my_map)
 my_map.save("map.html")
+
+
+
+##############################
+# Description of areas of interest using text pattern mining
+##############################
+
+# Preprocessing
+
+csv_file_to_be_processed = csv_file_clean_sample
+csv_file_processed = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_sample_processed.csv"
+
+def preprocessing(csv_file, csv_file_processed):
+    data = pd.read_table(csv_file, sep=",", low_memory=False)
+
+    # On supprime les lignes dont les tags et title sont vides
+    data = data.dropna(subset=['tags', 'title'])
+
+    # On supprime les tags qui ne sont pas des mots
+    data['tags'] = data['tags'].str.replace('[^a-zA-Z ]', '')
+
+    # On met tout en minuscule
+    data['tags'] = data['tags'].str.lower()
+
+    # On met les tags dans une liste
+    data['tags'] = data['tags'].str.split()
+
+    # On enlève les "stopwords" anglais
+    stopwords_english = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 'aren', 't', 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 'can', 'couldn', 't', 'd', 'did', 'didn', 't', 'do', 'does', 'doesn', 't', 'doing', 'don', 't', 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', 'hadn', 't', 'has', 'hasn', 't', 'have', 'haven', 't', 'having', 'he', 'her', 'here', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'isn', 't', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma', 'me', 'mightn', 't', 'more', 'most', 'mustn', 't', 'my', 'myself', 'needn', 't', 'no', 'nor', 'not', 'now', 'o', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 're', 's', 'same', 'shan', 't', 'she', 'should', 'should', 've']
+    
