65c26fa9967b8d5e6d0f5502289591cb7e99651b
Ereguof <v0.rddos@gmail.com>
2025-01-19 14:12:59 +0100

    finish tf_idf

--- datamining_project.py
+++ datamining_project.py
@@ -70,7 +70,7 @@ def cleaning(csv_file, csv_file_clean):
 
 # cleaning(csv_file, csv_file_clean)
 
-data = pd.read_table(csv_file_clean_sample, sep=",", low_memory=False)
+# data = pd.read_table(csv_file_clean_sample, sep=",", low_memory=False)
 
 #################################AFFICHAGE AVEC SEABORN
 # cols=['lat','long']
@@ -90,37 +90,145 @@ data = pd.read_table(csv_file_clean_sample, sep=",", low_memory=False)
 #AFFICHER LA CARTE ET UN MARQUEUR, POUR LA VOIR L'OUVRIR À LA MAIN DANS UN NAVIGATEUR
 ##############################
 
-my_map = folium.Map(location=(45.75, 4.83))
-for i in range(len(data)):
-    folium.CircleMarker(location=[data.at[i,"lat"], data.at[i,"long"]], tooltip="Afficher titre", popup=data.at[i,"title"], radius = 3).add_to(my_map)
-my_map.save("map.html")
+# my_map = folium.Map(location=(45.75, 4.83))
+# for i in range(len(data)):
+#     folium.CircleMarker(location=[data.at[i,"lat"], data.at[i,"long"]], tooltip="Afficher titre", popup=data.at[i,"title"], radius = 3).add_to(my_map)
+# my_map.save("map.html")
 
 
 
 ##############################
-# Description of areas of interest using text pattern mining
+# Description des zones d'intérêt en utilisant des techniques de text mining
 ##############################
 
-# Preprocessing of tags and title
+# Librairies for text mining
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
-csv_file_to_be_processed = csv_file_clean_sample
-csv_file_processed = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_sample_processed.csv"
+# Preprocessing des données
+
+csv_file_processed = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_processed.csv"
+csv_file_processed_sample = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_processed-SAMPLE.csv"
 
 def preprocessing(csv_file, csv_file_processed):
     data = pd.read_table(csv_file, sep=",", low_memory=False)
 
-    # On supprime les lignes dont les tags et title sont vides
-    data = data.dropna(subset=['tags', 'title'])
-
-    # On supprime les tags et title qui ne sont pas des mots
-    data['tags'] = data['tags'].str.replace(r'[^a-zA-Z ]', '')
-    data['title'] = data['title'].str.replace(r'[^a-zA-Z ]', '')
+    # On enlève les caractères spéciaux et les chiffres dans tags et title, mais pas les accents ou autres caractères courants
+    data['tags'] = data['tags'].str.replace(r'[^a-zA-Z éèôàëêöâäîïùûüç]', ' ', regex=True)
+    data['title'] = data['title'].str.replace(r'[^a-zA-Z éèôàëêöâäîïùûüç]', ' ', regex=True)
 
     # On met tout en minuscule
     data['tags'] = data['tags'].str.lower()
+    data['title'] = data['title'].str.lower()
 
-    # On met les tags dans une liste
-    data['tags'] = data['tags'].str.split()
+    # Lorsque tags ou title est de longueur 0, on le remplace par le contenu de l'autre
+    data['tags'].fillna('', inplace=True)
+    data['title'].fillna('', inplace=True)
 
-    # On enlève les "stopwords" anglais
-    stopwords_english = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 'aren', 't', 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 'can', 'couldn', 't', 'd', 'did', 'didn', 't', 'do', 'does', 'doesn', 't', 'doing', 'don', 't', 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', 'hadn', 't', 'has', 'hasn', 't', 'have', 'haven', 't', 'having', 'he', 'her', 'here', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'isn', 't', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma', 'me', 'mightn', 't', 'more', 'most', 'mustn', 't', 'my', 'myself', 'needn', 't', 'no', 'nor', 'not', 'now', 'o', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 're', 's', 'same', 'shan', 't', 'she', 'should', 'should', 've']
+    # On enlève les "stopwords" anglais et français dans title 
+    stop_words = set(stopwords.words('english'))
+    stop_words_fr = set(stopwords.words('french'))
+    data['title'] = data['title'].apply(lambda x: ' '.join([item for item in x.split() if item not in stop_words]))
+    data['title'] = data['title'].apply(lambda x: ' '.join([item for item in x.split() if item not in stop_words_fr]))
+
+    # On enlève les mots trop fréquents ou qui les contiennent ou lettres seules dans title et tags
+    frequent_words = ['photo', 'picture', 'image', 'photography', 'photograph', 'photographie','lyon', 'france', 'flickr', 'photographer', 'photographie', 'streetphotography','iphone','lesphotosdevoyage']
+    solo_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    data['title'] = data['title'].apply(lambda x: ' '.join([item for item in x.split() if item not in frequent_words and item not in solo_letters]))
+    data['tags'] = data['tags'].apply(lambda x: ' '.join([item for item in x.split() if item not in frequent_words and item not in solo_letters]))
+
+    # On refait un cleaning des data
+    data['tags'].fillna('', inplace=True)
+    data['title'].fillna('', inplace=True)
+
+    # On met les données dans un fichier csv
+    data.to_csv(csv_file_processed, index=False, sep=",")
+
+# Création d'un word cloud avec les données preprocessées
+
+def wordcloud(csv_file_processed):
+    data = pd.read_table(csv_file_processed, sep=",", low_memory=False)
+
+    data['tags'].fillna('', inplace=True)
+    data['title'].fillna('', inplace=True)
+
+    # On crée une liste de mots avec les titres et les tags
+    words = []
+    for i in range(len(data)):
+        words += data.at[i, 'title'].split()
+        words += data.at[i, 'tags'].split()
+
+    # On crée un word cloud
+    wordcloud = WordCloud(width = 800, height = 800,
+                background_color ='white',
+                min_font_size = 10).generate(' '.join(words))
+
+    plt.figure(figsize = (8, 8), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+    plt.show()
+
+
+# On suppose que le fichier a été nettoyé, prétraité et avec une colonne d'entiers représentant à quel cluster appartient chaque ligne
+# On cherche donc maintenant à trouver les meilleurs mots pour décrire chaque cluster en utilisant la méthode "term frequency and inverse document frequency"
+
+def TF_IDF(csv_file_processed, nb_clusters):
+    data = pd.read_table(csv_file_processed, sep=",", low_memory=False)
+
+    # On garantit que les tags et les titres sont bien des strings
+    data['tags'] = data['tags'].astype(str)
+    data['title'] = data['title'].astype(str)
+
+    # On commence par joindre les tags et les titres dans une nouvelle colonne puis on tokenise
+    data['text'] = data['title'] + ' ' + data['tags']
+    data['text'] = data['text'].apply(lambda x: word_tokenize(x))
+    
+    # On crée un dictionnaire avec les mots et leur fréquence
+    words = {}
+    for i in range(len(data)):
+        for word in data.at[i, 'text']:
+            if word in words:
+                words[word] += 1
+            else:
+                words[word] = 1
+    
+    # On crée un dictionnaire avec les mots et leur fréquence par cluster
+    words_by_cluster = {}
+    for i in range(nb_clusters):
+        words_by_cluster[i] = {}
+    for i in range(len(data)):
+        for word in data.at[i, 'text']:
+            if word in words_by_cluster[data.at[i, 'cluster']]:
+                words_by_cluster[data.at[i, 'cluster']][word] += 1
+            else:
+                words_by_cluster[data.at[i, 'cluster']][word] = 1
+
+    # On calcule le TF-IDF pour chaque mot
+    tf_idf = {}
+    for i in range(nb_clusters):
+        tf_idf[i] = {}
+    for i in range(len(data)):
+        for word in data.at[i, 'text']:
+            tf = words_by_cluster[data.at[i, 'cluster']][word] / len(data)
+            idf = len(data) / words[word]
+            tf_idf[data.at[i, 'cluster']][word] = tf * idf
+
+    # On affiche les mots les plus importants pour chaque cluster
+    for i in range(nb_clusters):
+        tf_idf[i].pop('nan')
+        print("Cluster", i)
+        print(sorted(tf_idf[i], key=tf_idf[i].get, reverse=True)[:10])
+ 
+
+# preprocessing(csv_file_clean_sample, csv_file_processed_sample)
+# wordcloud(csv_file_processed_sample)
+
+csv_file_processed_sample_cluster = "C:/Users/felzi/Desktop/INSA/4IF/S1/DataMining/flickr_data_processed-SAMPLE_fake_clusters.csv"
+
+TF_IDF(csv_file_processed_sample_cluster, 3)
